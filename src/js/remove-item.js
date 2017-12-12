2f5897949fadc39bf9b5f1b7d6feff6da9f19027
Rakibullah Sazib <rakibullahsazib@yahoo.com>
2017-12-12 20:16:49 +0600

    Add Sure Remove prompt box

--- src/js/remove-item.js
+++ src/js/remove-item.js
@@ -6,11 +6,43 @@ import {hideDetails} from './hide-details';
 import {addRecipeF} from "./add-recipe";
 
 
-export function removeItem (itemNo){
-    hideDetails(itemNo);
-    recipes.splice(itemNo,1);
-    console.log(recipes);
-    
-    addRecipeF();
+class SureRemove extends React.Component{   
 
+    constructor(props){
+        super(props);
+        this.clearForm = this.clearForm.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    deleteItem(item){
+        hideDetails(item);
+        recipes.splice(item,1);
+        console.log(recipes);
+        this.clearForm();
+        addRecipeF();
+    }
+    clearForm(){
+        $("#curtain").attr("hidden","hidden");
+    }
+    render(){
+            
+        return (
+            <div id="form">
+                <div>Are you sure you want to delete this recipe</div>
+                <div>
+                    <button onClick={()=>this.deleteItem(this.props.itemNo)}>yes</button>
+                    <button onClick={this.clearForm}>no</button>
+                </div>
+            </div> 
+        );
+        
+    }
+}
+
+
+export function removeItem(i){
+
+    $("#curtain").removeAttr("hidden");
+    ReactDOM.render(<SureRemove itemNo={i} />, document.getElementById("curtain")); 
+    
 }
